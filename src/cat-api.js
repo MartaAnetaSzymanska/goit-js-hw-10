7e653b60b1ec6035368a712f29fc653ecc4d30dc
MartaAnetaSzymanska <szymanska.ma@gmail.com>
2024-04-22 21:55:02 +0200

    add_notifications

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,36 +2,11 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_VphXHW9UfClNGoRVrJb3ygzZ44h0TOe7bIfGB21lWwSeP86oJJzv8AgjerpfnDgr';
 
-export const select = document.querySelector('select.breed-select');
-export const breedDetailsBox = document.querySelector('div.cat-info');
-export const loadingNotification = document.querySelector('p.loader');
-export const errorNotification = document.querySelector('p.error');
-
 export function fetchBreeds() {
-  loadingNotification.hidden = false;
-  return fetch('https://api.thecatapi.com/v1/breeds')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.text);
-      }
-      return response.json();
-    })
-    .then(breeds => {
-      // console.log(breeds);
-      for (let i = 0; i < breeds.length; i++) {
-        let option = document.createElement('option');
-        option.value = breeds[i].id;
-        option.innerText = breeds[i].name;
-        select.append(option);
-      }
-      loadingNotification.hidden = true;
-      select.hidden = false;
-    })
-    .catch(error => {
-      console.log('error', error);
-    });
+  return axios.get('https://api.thecatapi.com/v1/breeds').then(breeds => {
+    return breeds.data;
+  });
 }
-
 export function fetchCatByBreed(breedId) {
   return axios
     .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
--- src/index.js
+++ src/index.js
@@ -1,34 +1,66 @@
 import axios from 'axios';
-import {
-  fetchBreeds,
-  select,
-  breedDetailsBox,
-  fetchCatByBreed,
-  fetchCatDetails,
-  loadingNotification,
-  errorNotification,
-} from './cat-api';
+import { fetchBreeds, fetchCatByBreed, fetchCatDetails } from './cat-api';
 
+const select = document.querySelector('select.breed-select');
+const breedDetailsBox = document.querySelector('div.cat-info');
+const loader = document.querySelector('p.loader');
+const error = document.querySelector('p.error');
 const catInfo = document.querySelector('div.cat-info');
 
-fetchBreeds();
+select.classList.add('hidden');
+error.classList.add('hidden');
+
+fetchBreeds()
+  .then(data => {
+    loader.classList.add('hidden');
+    select.classList.remove('hidden');
+    for (let i = 0; i < data.length; i++) {
+      let option = document.createElement('option');
+      option.value = data[i].id;
+      option.innerText = data[i].name;
+      select.append(option);
+    }
+  })
+  .catch(err => {
+    loader.classList.add('hidden');
+    select.classList.add('hidden');
+    error.classList.remove('hidden');
+    console.log('error', err);
+    throw error;
+  });
 
 select.addEventListener('change', ev => {
+  catInfo.classList.add('hidden');
+  loader.classList.remove('hidden');
   let breedId = ev.target.value;
   // console.log(breedId);
-  loadingNotification.hidden = false;
   fetchCatByBreed(breedId)
-    .then(data => {
-      let breedObj = data[0];
-      breedDetailsBox.innerHTML = `<img src="${breedObj.url}" width="300" height="250"></img>`;
+    .then(cats => {
+      let cat = cats[0];
+      breedDetailsBox.innerHTML = `<img src="${cat.url}" alt ="cat"></img>`;
+      catInfo.classList.remove('hidden');
     })
-    .catch(error => {});
+    .catch(err => {
+      loader.classList.add('hidden');
+      select.classList.add('hidden');
+      error.classList.remove('hidden');
+      console.log('error', err);
+      throw err;
+    });
 
-  fetchCatDetails(breedId).then(data => {
-    let breedObj = data.find(el => el.id === breedId);
-    const markup = `<div><h2>${breedObj.name}</h2><p>${breedObj.description}</p><span>Temperament:</span>${breedObj.temperament}<p></p></div>`;
-    breedDetailsBox.insertAdjacentHTML('beforeend', markup);
-  });
-  loadingNotification.hidden = true;
-  catInfo.hidden = false;
+  fetchCatDetails(breedId)
+    .then(cats => {
+      let cat = cats.find(el => el.id === breedId);
+      const markup = `<div><h2 class="title">${cat.name}</h2><p>${cat.description}</p><span class="span-temper">Temperament: </span>${cat.temperament}</div>`;
+      breedDetailsBox.insertAdjacentHTML('beforeend', markup);
+      loader.classList.add('hidden');
+      catInfo.classList.remove('hidden');
+    })
+    .catch(err => {
+      loader.classList.add('hidden');
+      select.classList.add('hidden');
+      error.classList.remove('hidden');
+      console.log('error', err);
+      throw error;
+    });
 });
